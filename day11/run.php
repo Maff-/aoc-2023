16ad4d1e29f4437d65d3fac1f0a0e259643300ad
Ruud Bijnen <r.bijnen@gmail.com>
2023-12-11 19:20:36 +0100

    Day 11 part 2

--- day11/run.php
+++ day11/run.php
@@ -75,3 +75,62 @@ for ($i = 0, $iMax = count($galaxies); $i < $iMax; $i++) {
 }
 
 echo 'Part 1: ', $sum, \PHP_EOL;
+
+// Part 2 - Copy & Paste & Tweak
+
+$foo = 1_000_000 - 1;
+
+$map = $input;
+$height = count($map);
+$originalWidth = count($map[0]);
+$width = $originalWidth;
+$emptyCols = [];
+for ($x = 0, $nX = 0; $x < $originalWidth; $x++, $nX++) {
+    $emptyCol = true;
+    foreach ($map as $y => $row) {
+        if ($row[$x] !== '.') {
+            $emptyCol = false;
+        }
+    }
+    $emptyCols[$nX] = $emptyCol;
+    if ($emptyCol) {
+        $nX += $foo;
+        $width += $foo;
+    }
+}
+
+$y = 0;
+$galaxies = [];
+foreach ($map as $row) {
+    $emptyRow = true;
+    $x = 0;
+    foreach ($row as $col) {
+        if ($col === '#') {
+            $emptyRow = false;
+            $galaxies[] = [$x, $y];
+        }
+        $x++;
+        if ($emptyCols[$x] ?? false) {
+            $x += $foo;
+        }
+    }
+    $y++;
+    if ($emptyRow) {
+        $y += $foo;
+        $height += $foo;
+    }
+}
+
+$sum = 0;
+$n = 0;
+for ($i = 0, $iMax = count($galaxies); $i < $iMax; $i++) {
+    for ($j = $i + 1; $j < $iMax; $j++) {
+        $a = $galaxies[$i];
+        $b = $galaxies[$j];
+        $dist = abs($b[0] - $a[0]) + abs($b[1] - $a[1]);
+        $n++;
+        $sum += $dist;
+    }
+}
+
+echo 'Part 2: ', $sum, \PHP_EOL;
