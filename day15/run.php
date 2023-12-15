66f079d080415266c960bfc3dfaac3977bdd67e1
Ruud Bijnen <r.bijnen@gmail.com>
2023-12-15 13:44:16 +0100

    Day 15 part 2

--- day15/run.php
+++ day15/run.php
@@ -29,3 +29,39 @@ assert(_hash('HASH') === 52);
 $result = array_sum(array_map('_hash', $input));
 
 echo 'Part 1: ', $result, \PHP_EOL;
+
+// Part 2
+
+$boxes = array_fill(0, 256, [[], []]);
+
+foreach ($input as $instruction) {
+    [, $label, $operation, $focalLength] = preg_match('/^(\w+)([-=])(\d*)$/', $instruction, $match) ? $match : throw new \RuntimeException("Regex did not match '{$instruction}'");
+    $n = _hash($label);
+    switch ($operation) {
+        case '-':
+            // remove lens
+            if (isset($boxes[$n][0][$label])) {
+                $pos = array_search($label, $boxes[$n][1], true);
+                array_splice($boxes[$n][1], $pos, 1);
+                unset($boxes[$n][0][$label]);
+            }
+            break;
+        case '=':
+            // add/replace lens
+            if (!isset($boxes[$n][0][$label])) {
+                $boxes[$n][1][] = $label;
+            }
+            $boxes[$n][0][$label] = (int)$focalLength;
+            break;
+    }
+}
+
+$result = null;
+foreach ($boxes as $n => $box) {
+    foreach ($box[1] as $slot => $label) {
+        $focalLength = $box[0][$label];
+        $result += ($n + 1) * ($slot + 1) * $focalLength;
+    }
+}
+
+echo 'Part 2: ', $result, \PHP_EOL;
