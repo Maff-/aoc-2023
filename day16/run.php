8a13914669a588b384fd5bfe5392bdcc74e75e7f
Ruud Bijnen <r.bijnen@gmail.com>
2023-12-16 12:20:43 +0100

    Day 16; functionalize part 1

--- day16/run.php
+++ day16/run.php
@@ -25,64 +25,70 @@ $width = count($input[0]);
 
 // Part 1
 
-$energized = [];
-$directions = [$north, $east, $south, $west] = [[0, -1], [1, 0], [0, 1], [-1, 0]];
-$directionNames = ['N', 'E', 'S', 'W'];
-$processed = [];
-$queue = [[[0, 0], $east]];
+function energize(array $input, array $start): int
+{
+    global $height, $width;
+    $directions = [$north, $east, $south, $west] = [[0, -1], [1, 0], [0, 1], [-1, 0]];
 
-while ($queue) {
-    [$pos, $direction] = array_shift($queue);
-    [$x, $y] = $pos;
-    [$dX, $dY] = $direction;
-    if ($x < 0 || $x >= $width || $y < 0 || $y >= $height) {
-        // out of bounds
-        continue;
-    }
-    if (isset($processed[$y][$x]) && in_array($direction, $processed[$y][$x], true)) {
-        // already done
-        continue;
-    }
-    $processed[$y][$x][] = $direction;
-    $cell = $input[$y][$x];
-    $energized[$y][$x] = true;
-    switch ($cell) {
-        case '.':
-            $queue[] = [[$x + $dX, $y + $dY], $direction];
-            break;
-        case '|':
-            if ($dX === 0) {
-                // pointy end; continue
+    $energized = [];
+    $processed = [];
+    $queue = [$start];
+
+    while ($queue) {
+        [$pos, $direction] = array_shift($queue);
+        [$x, $y] = $pos;
+        [$dX, $dY] = $direction;
+        if ($x < 0 || $x >= $width || $y < 0 || $y >= $height) {
+            // out of bounds
+            continue;
+        }
+        if (isset($processed[$y][$x]) && in_array($direction, $processed[$y][$x], true)) {
+            // already done
+            continue;
+        }
+        $processed[$y][$x][] = $direction;
+        $cell = $input[$y][$x];
+        $energized[$y][$x] = true;
+        switch ($cell) {
+            case '.':
                 $queue[] = [[$x + $dX, $y + $dY], $direction];
-            } else {
-                // split
-                $queue[] = [[$x, $y - 1], $north];
-                $queue[] = [[$x, $y + 1], $south];
-            }
-            break;
-        case '-':
-            if ($dY === 0) {
-                // pointy end; continue
-                $queue[] = [[$x + $dX, $y + $dY], $direction];
-            } else {
-                // split
-                $queue[] = [[$x - 1, $y], $west];
-                $queue[] = [[$x + 1, $y], $east];
-            }
-            break;
-        case '/':
-        case '\\':
-            $directionIndex = array_search($direction, $directions, true);
-            $indexMod = ($cell === '/' && $dY === 0) || ($cell === '\\' && $dX === 0) ? -1 : 1;
-            $turnIndex = (4 + $directionIndex + $indexMod) % 4;
-            $turn = [$dX, $dY] = $directions[$turnIndex];
-            $queue[] = [[$x + $dX, $y + $dY], $turn];
-            break;
-        default:
-            throw new \RuntimeException("Unhandled cell type '{$cell}'");
+                break;
+            case '|':
+                if ($dX === 0) {
+                    // pointy end; continue
+                    $queue[] = [[$x + $dX, $y + $dY], $direction];
+                } else {
+                    // split
+                    $queue[] = [[$x, $y - 1], $north];
+                    $queue[] = [[$x, $y + 1], $south];
+                }
+                break;
+            case '-':
+                if ($dY === 0) {
+                    // pointy end; continue
+                    $queue[] = [[$x + $dX, $y + $dY], $direction];
+                } else {
+                    // split
+                    $queue[] = [[$x - 1, $y], $west];
+                    $queue[] = [[$x + 1, $y], $east];
+                }
+                break;
+            case '/':
+            case '\\':
+                $directionIndex = array_search($direction, $directions, true);
+                $indexMod = ($cell === '/' && $dY === 0) || ($cell === '\\' && $dX === 0) ? -1 : 1;
+                $turnIndex = (4 + $directionIndex + $indexMod) % 4;
+                $turn = [$dX, $dY] = $directions[$turnIndex];
+                $queue[] = [[$x + $dX, $y + $dY], $turn];
+                break;
+            default:
+                throw new \RuntimeException("Unhandled cell type '{$cell}'");
+        }
     }
+
+    return array_sum(array_map('array_sum', $energized));
 }
 
-$result = array_sum(array_map('array_sum', $energized));
+$result = energize($input, [[0, 0], [1, 0]]);
 
 echo 'Part 1: ', $result, \PHP_EOL;
